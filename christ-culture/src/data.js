d39ade0e4de80c0bbfd45652908b07c3b34047f0
kev-69 <bismarkobuobi19@gmail.com>
2025-03-10 21:03:38 +0000

    sample data for cloths

---
+++ christ-culture/src/data.js
@@ -0,0 +1,150 @@
+import salvationfront from './assets/apparels/salvation_front.png'
+import salvationback from './assets/apparels/salvation_back.png'
+import keepprayingfront from './assets/apparels/keep_praying_front.png'
+import keepprayingback from './assets/apparels/keep_praying_back.png'
+import holyspiritfront from './assets/apparels/holy_spirit_front.png'
+import holyspiritback from './assets/apparels/holy_spirit_back.png'
+import godsplanfront from './assets/apparels/gods_plan_front.png'
+import godsplanback from './assets/apparels/gods_plan_back.png'
+import eternalfaithfront from './assets/apparels/eternal_faith_front.png'
+import eternalfaithback from './assets/apparels/eternal_faith_back.png'
+import empoweredfront from './assets/apparels/empowered_front.png'
+import empoweredback from './assets/apparels/empowered_back.png'
+import disciplesfront from './assets/apparels/disciples_front.png'
+import disciplesback from './assets/apparels/disciples_back.png'
+import christstrengthfront from './assets/apparels/christ_strenght_front.png'
+import christstrengthback from './assets/apparels/christ_strenght_back.png'
+import chosenfront from './assets/apparels/chosen_front.png'
+import chosenback from './assets/apparels/chosen_back.png'
+import breadoflifefront from './assets/apparels/bread_of_life_front.png'
+import breadoflifeback from './assets/apparels/bread_of_life_back.png'
+import angelsondutyfront from './assets/apparels/angels_on_duty_front.png'
+import angelsondutyback from './assets/apparels/angels_on_duty_back.png'
+// import disciplesback from './assets/apparels/salvation_front.jpg'
+
+
+const ApparelsData = [
+    {
+        id: 1,
+        title: "Angels On Duty",
+        price: 180,
+        images: {
+            front: angelsondutyfront,
+            back: angelsondutyback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 2,
+        title: "Bread Of Life",
+        price: 180,
+        images: {
+            front: breadoflifefront,
+            back: breadoflifeback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 3,
+        title: "Chosen Generation",
+        price: 180,
+        images: {
+            front: chosenfront,
+            back: chosenback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 4,
+        title: "Christ Strenght",
+        price: 180,
+        images: {
+            front: christstrengthfront,
+            back: christstrengthback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 5,
+        title: "Disciples",
+        price: 180,
+        images: {
+            front: disciplesfront,
+            back: disciplesback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 6,
+        title: "Empowered Through Christ",
+        price: 180,
+        images: {
+            front: empoweredfront,
+            back: empoweredback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 7,
+        title: "Eternal Faith",
+        price: 180,
+        images: {
+            front: eternalfaithfront,
+            back: eternalfaithback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 8,
+        title: "God's Plan",
+        price: 180,
+        images: {
+            front: godsplanfront,
+            back: godsplanback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 9,
+        title: "Holy Spirit",
+        price: 180,
+        images: {
+            front: holyspiritfront,
+            back: holyspiritback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 10,
+        title: "Keep Praying",
+        price: 180,
+        images: {
+            front: keepprayingfront,
+            back: keepprayingback
+        },
+        isInStock: true,
+        discount: 0
+    },
+    {
+        id: 11,
+        title: "Salvation",
+        price: 180,
+        images: {
+            front: salvationfront,
+            back: salvationback
+        },
+        isInStock: true,
+        discount: 0
+    },
+];
+
+export { ApparelsData }
