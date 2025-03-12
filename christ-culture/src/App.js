5107f79de2ec9f66953540309eb9807286dd800f
kev-69 <bismarkobuobi19@gmail.com>
2025-03-12 09:47:33 +0000

    implemented the whatsapp icon feature

--- christ-culture/src/App.js
+++ christ-culture/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
 import Layout from './components/Layout'
+import WhatsAppButton from "./components/WhatsappButton";
 
 import Home from './pages/Home'
 import Shop from "./pages/Shop";
@@ -16,6 +17,8 @@ function App() {
               <Route path="/shop" element={<Shop />} />
               <Route path="/collections" element={<Collections />} />
             </Routes>
+            <WhatsAppButton phoneNumber="+233257677987" message="Hello! I'd like to
+            make an enquiry about your products." />
           </Layout>
       </div>
    </Router>
